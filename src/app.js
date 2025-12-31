6909e94363e7f233d1d46563ac06aead6ca30c06
kk3069kk <kislaykk.work@gmail.com>
2025-12-31 20:20:01 +0530

    database connection

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import express from "express";
+import mongodb from "./config/database.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:'./env'
+})
 
 const app = express();
 
-app.get("/kk" ,(req,res) =>{
-    res.send("Hello world");
-})
+mongodb()
+.then(()=> console.log("coneected"))
+.catch((err)=>console.log(err));
+
 
 app.listen(7777 , ()=>{
     console.log("app is litening");
 })
+
