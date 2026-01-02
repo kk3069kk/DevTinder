1fb671b4c585b1e041cb7612b72093dc3dda802b
kk3069kk <kislaykk.work@gmail.com>
2026-01-02 23:31:36 +0530

    making routers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongodb from "./config/database.js";
+import User from "./models/user.model.js";
 import dotenv from "dotenv";
 
 dotenv.config({
@@ -7,6 +8,45 @@ dotenv.config({
 })
 
 const app = express();
+app.use(express.json());
+
+app.post("/signup" , async (req,res)=>{
+    
+    const user = new User(req.body);
+    try {
+        await user.save();
+        res.send("Success");
+    } catch (error) {
+        res.status(401).send("ERROR" + error);
+    }
+
+})
+
+app.get("/user" , async (req,res)=>{
+    const userEmail = req.body.emailId;
+
+    try {
+
+     const user =   await User.find({emailId:userEmail});
+
+     if(user.length === 0) res.status(404).send("User not found");
+     res.send(user);
+
+    } catch (error) {
+        res.status(401).send("something went wrong");
+    }
+})
+
+app.get("/getalluser" , async (req,res)=>{
+
+    try {
+     const user =   await User.find({});
+     res.status(200).send(user);
+
+    } catch (error) {
+        res.status(401).send("something went wrong");
+    }
+})
 
 mongodb()
 .then(()=> {
